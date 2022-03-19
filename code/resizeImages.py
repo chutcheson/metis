fe285bd98c46cc5bb10964ed169b8a51dd12ee53
Campbell Hutcheson <nchutcheson@gmail.com>
2022-03-19 01:07:23 +0000

    resize images

--- code/resizeImages.py
+++ code/resizeImages.py
@@ -1,8 +1,10 @@
 from PIL import Image
-from config import IMAGE_TABLE
+from config import DATA, IMAGE_TABLE
+from collections import defaultdict
 from csv import reader
+from pathlib import Path
 
-def centerCrop(fileName):
+def centerCrop(fileName, new_height, new_width):
 
     # open image file
     im = Image.open(fileName)
@@ -17,3 +19,34 @@ def centerCrop(fileName):
     im = im.crop((left, top, right, bottom))
 
     return im
+
+vaseImages = defaultdict(list)
+
+with open(IMAGE_TABLE, "r") as f:
+
+    for image in reader(f):
+
+        if int(image[4]) < 244 or int(image[5]) < 244 or int(image[6]) != 3:
+
+            pass
+
+        else:
+
+            vaseImages[image[0]].append(image[1])
+
+cropPath = Path(DATA + "croppedImages")
+
+cropPath.mkdir(exist_ok=True)
+
+for key in vaseImages:
+
+    vasePath = Path(DATA + "croppedImages/" + key)
+
+    vasePath.mkdir(exist_ok=True)
+
+    for image in vaseImages[key]:
+
+        im = centerCrop(image, 244, 244)
+
+        im.save(DATA + "croppedImages/" + key + "/" + image.split("/")[-1])
+
