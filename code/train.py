faa6fdd0952b2ff1cd9ca303d33fd5db4ebca7bc
Campbell Hutcheson <nchutcheson@gmail.com>
2022-03-21 20:31:12 +0000

    changed train

--- code/train.py
+++ code/train.py
@@ -1,16 +1,20 @@
 from config import KERAS_IMAGES
 from pathlib import Path
 from tensorflow.keras.utils import image_dataset_from_directory
-from tensorflow.keras import models
-from tensorflow.keras.layers import Flatten, Dense, Dropout
-from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
-from tensorflow.keras.applications import MobileNetV2
-from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Flatten, Dense, Conv2D, MaxPooling2D, Flatten, InputLayer, Dropout
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.regularizers import l2
 
-def preprocess(images, labels):
-  return preprocess_input(images), labels
+t = image_dataset_from_directory(
+    KERAS_IMAGES, labels='inferred', label_mode='binary',
+    class_names=None, color_mode='rgb', batch_size=32, image_size=(224,
+    224), shuffle=True, seed=34829437, validation_split=.2, subset="training", 
+    interpolation='bilinear', follow_links=False,
+    crop_to_aspect_ratio=False
+)
 
-vaseDataset = image_dataset_from_directory(
+v = image_dataset_from_directory(
     KERAS_IMAGES, labels='inferred', label_mode='binary',
     class_names=None, color_mode='rgb', batch_size=32, image_size=(224,
     224), shuffle=True, seed=34829437, validation_split=.2, subset="validation", 
@@ -18,26 +22,18 @@ vaseDataset = image_dataset_from_directory(
     crop_to_aspect_ratio=False
 )
 
-vaseDataset = vaseDataset.map(preprocess)
-
-model = models.Sequential()
-
-base_model = MobileNetV2(weights='imagenet',include_top=False,input_shape=(224,224,3))
-
-for layer in base_model.layers[:-4]:
-    
-    layer.trainable = False
-
-model.add(base_model)
-
+model = Sequential()
+model.add(InputLayer(input_shape=(224,224,3)))
+model.add(Conv2D(filters=20, kernel_size=3, activation='relu',kernel_regularizer=l2(1e-5)))
+model.add(MaxPooling2D())
+model.add(Conv2D(filters=20, kernel_size=3, activation='relu',kernel_regularizer=l2(1e-5)))
+model.add(MaxPooling2D())
+model.add(Dropout(.2))
 model.add(Flatten())
+model.add(Dropout(.2))
+model.add(Dense(100,activation='relu'))
+model.add(Dense(1,activation='sigmoid'))
 
-model.add(Dense(1024, activation = 'relu'))
+model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
 
-model.add(Dropout(0.5))
-
-model.add(Dense(1, activation='sigmoid'))
-
-model.compile(loss='binary_crossentropy',optimizer=Adam(lr=0.0001),metrics=['accuracy'])
-
-model.fit(vaseDataset,batch_size=64)
+model.fit(t,validation_data=v,epochs=25,callbacks=[EarlyStopping(patience=3)])
