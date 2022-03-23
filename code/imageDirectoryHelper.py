137424ac415f74e5702c9ea56c39c11ad1e1b411
Campbell Hutcheson <nchutcheson@gmail.com>
2022-03-23 13:45:00 +0000

    more functions

---
+++ code/imageDirectoryHelper.py
@@ -0,0 +1,67 @@
+from pathlib import Path
+from numpy import asarray, array_equal
+from collections import defaultdict
+from PIL import Image
+
+# getVaseImages takes a directory with subdirectories for each vase
+# getVase returns a dictionary of vase : List[images]
+
+def getVaseImages(directory):
+
+    # create dict to store IDs of vases with images and list of file paths
+    vases = defaultdict(list)
+
+    # create path object for images folder
+    imagesFolder = Path(directory)
+
+    # iterate over the folder representing each vase
+    for path in imagesFolder.iterdir():
+
+        # get image files
+        for f in path.glob("**/*"):
+
+            # use identifier as key and add file path
+            vases[str(path).split("/")[-1]].append(str(f))
+
+    return vases
+
+def getImageChannelsAndColoration(imageFile):
+
+    # get image as numpy array
+    imageArray = asarray(Image.open(imageFile))
+
+    # set image height
+    height = imageArray.shape[0]
+    
+    # set image width
+    width = imageArray.shape[1]
+
+    # check if image has channels
+    if len(imageArray.shape) == 3:
+
+        # get image channels
+        channels = imageArray.shape[2]
+
+        if array_equal(imageArray[:,:,0], imageArray[:,:,1]) and array_equal(imageArray[:,:,0], imageArray[:,:,2]):
+
+            # if all channels are equal then image is grayscale
+            color = "GRAYSCALE"
+
+        else:
+
+            # otherwise image is color
+            color = "COLOR"
+
+        return (channels, color)
+
+    else:
+
+        # if no channels
+        channels = 0
+        
+        # if no channels then image is grayscale
+        color = "GRAYSCALE"
+
+        return (channels, color)
+
+
--- code/tableHelper.py
+++ code/tableHelper.py
@@ -1,4 +1,99 @@
+from xml.etree.ElementTree import parse
 from csv import reader, writer
+
+# takes a path to XML document and uses it to make a table
+
+def xmlToRecords(manifest):
+
+    # create list to store records of vase images
+    vaseRecords = []
+
+    # create XML tree of vases
+    tree = parse(manifest)
+
+    # iterate over vases
+    for vase in tree.getroot():
+
+        # initialize technique field
+        technique = None
+        
+        # initialize provenance field
+        provenance = None
+
+        # check for vase id
+        if not vase.attrib:
+
+            # continue if no id
+            continue
+
+        # remove brackets around vase id
+        vaseID = vase.attrib['id'].lstrip("{").rstrip("}")
+
+        # check to see that there are images for vase
+        if vaseID not in vases:
+
+            # continue if no images
+            continue
+
+        # iterate over vase attributes
+        for child in vase:
+
+            # check for technique
+            if child.tag == "Technique":
+
+                technique = child.text
+
+            # check for provenance
+            elif child.tag == "Provenance":
+
+                provenance = child.text
+
+            else:
+
+                pass
+
+        # if all fields were present 
+        if technique and provenance:
+
+            # iterate over vase images
+            for image in vases[vaseID]:
+
+                # get image as numpy array
+                imageArray = asarray(Image.open(image))
+
+                # set image height
+                height = imageArray.shape[0]
+                
+                # set image width
+                width = imageArray.shape[1]
+
+                # check if image has channels
+                if len(imageArray.shape) == 3:
+
+                    # get image channels
+                    channels = imageArray.shape[2]
+
+                    if array_equal(imageArray[:,:,0], imageArray[:,:,1]) and array_equal(imageArray[:,:,0], imageArray[:,:,2]):
+
+                        # if all channels are equal then image is grayscale
+                        color = "GRAYSCALE"
+
+                    else:
+
+                        # otherwise image is color
+                        color = "COLOR"
+
+                else:
+
+                    # if no channels
+                    channels = 0
+                    
+                    # if no channels then image is grayscale
+                    color = "GRAYSCALE"
+
+                # add a row to vaseFileTable with vase info
+                vaseFileTable.append([vaseID, image, technique, provenance.split(",")[0].split(" ")[0], height, width, channels, color])
+
 
 # takes a path to a table
 # returns a list of lists where each list stores image metadata
